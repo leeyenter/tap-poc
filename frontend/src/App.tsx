e364df86d92c81d2c52acf03226d3bd2dcd0f717
Yen Ter Lee <yen-ter.lee@broadcom.com>
2024-01-15 17:24:50 +0800

    Trigger commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <h1>Counter</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is {count}!
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
